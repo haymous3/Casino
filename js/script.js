833041224333955be5d6ce6e2de5d5edfb08a46b
Amos <adeyemoamos33@gmail.com>
2023-03-08 04:49:39 -0800

    integrating game logic

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 
-const selectImageOne = document.querySelector('.select_image_one');
-const selectImageTwo = document.querySelector('.select_image_two');
+const selectImageOne = document.querySelector('.game_center_right_imgs_one');
+const selectImageTwo = document.querySelector('.game_center_right_imgs_two');
+const playNow = document.querySelector('#playnow');
+const imageHead = document.querySelector('#imagehead');
+const imageTail = document.querySelector('#imagetail')
 
 
 const LoadEventListerners = () => {
     selectImageOne.addEventListener('click', selectHead);
-    selectImageTwo.addEventListener('click', selectTail)
+    selectImageTwo.addEventListener('click', selectTail);
+    playNow.addEventListener('click', startgame)
 
 }
 
@@ -20,6 +24,28 @@ const selectHead = () => {
 const selectTail = () => {
     selectImageOne.classList.remove('active')
     selectImageTwo.classList.add('active')
+
+}
+
+const generateTwoRandomNumbers = (min, max) => {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+const startgame = () => {
+  
+
+
+setTimeout(() => {
+    if(generateTwoRandomNumbers(1, 2) === 1){
+        imageHead.classList.add('display')
+        console.log(generateTwoRandomNumbers(1,2))
+    }else{
+        imageTail.classList.add('display')
+    }
+}, 1000);
+ 
+
 
 }
 
