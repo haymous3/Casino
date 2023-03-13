62ab41c66427bf2e7221e894b7083fbf75758aaf
Amos <adeyemoamos33@gmail.com>
2023-03-13 11:02:44 -0700

    50%

--- js/script.js
+++ js/script.js
@@ -3,186 +3,122 @@ const selectImageOne = document.querySelector('.game_center_right_imgs_one');
 const selectImageTwo = document.querySelector('.game_center_right_imgs_two');
 const playNow = document.querySelector('#playnow');
 const imageHead = document.querySelector('#imagehead');
+const gameCenterOne = document.querySelector('.game_center_left_one')
+const imageTail = document.querySelector('#imagetail');
+const selectImgHead = document.querySelector('#select_img_head');
+const selectImgTail = document.querySelector('#select_img_tail');
+// const play = document.querySelector('.play')
 
 
 
-
-let headSelect = '';
-let tailSelect = ''
 
 const LoadEventListerners = () => {
     selectImageOne.addEventListener('click', selectHead);
+    
     selectImageTwo.addEventListener('click', selectTail);
-    // selectSide.addEventListener('click', one)
-    // tailSelect.addEventListener('click', two)
+    selectImgHead.addEventListener('click', a)
+    selectImgTail.addEventListener('click', b)
    
-    
-
-}
-
-
-
-
-const selectHead = () => {
-    
-    selectImageOne.classList.add('active');
-    selectImageTwo.classList.remove('active');
-
-   return document.getElementsByClassName('game_center_right_imgs_one active');
-
-//    return headSelect
-
-}
-
-
-// headSelect = selectHead()
-
-
-const selectTail = () => {
-    selectImageOne.classList.remove('active');
-    selectImageTwo.classList.add('active');
-
-    return document.getElementsByClassName('game_center_right_imgs_two active');
-}
-
-// console.log(headSelect)
-// tailSelect = selectTail()
-
-const generateTwoRandomNumbers = (min, max) => {
-
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-const identifyUserClick = (head, tail) => {
-
-    if(head){
-        return 1
-    }else if(tail){
-        return 2
-    }
-    if(tail === false){ 
-        return 1
-    }else if(head === false){
-        return 2
-    }
-
-}
-
-
-
-const createImageHead = () => {
- 
-    const createHead = document.createElement('img')
-    createHead.src = 'images/head.png'
-    createHead.id = "myimg"
-    imageHead.appendChild(createHead)
-}
-
-const createImageTail = () => {
-    const createTail = document.createElement('img')
-    createTail.src = 'images/tail.png';
-
-    createTail.id ="myimg"
-
-    imageHead.appendChild(createTail)         
-}
-
-
-
- const resetgame = () => {
-    const deleteElement = document.getElementById('myimg')
-    return deleteElement.remove()
-
    
+}
 
-   
+
+let result;
+let results;
+
+const randomwinners = (probabilityRatio, computer, users) => {
+    return Math.random() < probabilityRatio ? computer : users
+}
+
+const resetgame = () => {
+    const deleteElement = document.querySelectorAll('.myimg')
+    return deleteElement.forEach((img) => img.remove())
+
 
  }
 
 
 
+const selectHead = (head) => {
+    
+    selectImageOne.classList.add('active')
+    selectImageTwo.classList.remove('active')
+   
+}
+
+const selectTail = () => {
+    
+    selectImageTwo.classList.add('active')
+    selectImageOne.classList.remove('active')
+ 
+}
+
+
+const a = () => {
+    result =  randomwinners(0.9, 1, 2)
+    console.log(result)
+   return result
+    
+}
+
+const b = () => {
+    results =  randomwinners(0.9, 1, 2)
+console.log(results)
+    return results
+}
+
+
+let myfunvtion = true
+
 const startgame = () => {
-    resetgame()
-    
 
-  
-     getNum = generateTwoRandomNumbers(1,2)
+gameCenterOne.style.display = 'none'
 
-     let choose = identifyUserClick(selectHead(), selectTail())
-
-     
-    if(getNum === 1){
-        createImageHead()
-    }else if(getNum === 2){
-        createImageTail()
-    }else{
-        null
+       if(result === 1 && myfunvtion){
+        alert('computer win')
+       return imageTail.style.display = 'block'
+       
+      
     }
+    else if(results === 1){
+        imageHead.style.display = 'block'
+        alert('computer win')
 
-    
-    console.log(choose)
-    console.log(getNum)
-    
-    if(choose === getNum){
-        alert('You won')
-    }else{
-        alert('try again')
+    }else{imageHead.style.display = 'block'
+        imageTail.style.display = 'block'
+        alert('you win')
     }
+   
 
+    
 }
 
 
 
-// const declearWinner = () => {
-
-//     switch (true) {
-//         case headSelect === getNum:
-//             alert('You win')
-//             break;
-//         case tailSelect === getNum:
-//             alert('You win')
-//             break;
-//         case headSelect < getNum:
-//             alert('You lose')
-//             break;
-//         case tailSelect > getNum:
-//             alert('You lose')
-//         default:
-//             break;
-//     }
-
-    
-    
-// }
 
 
 LoadEventListerners()
 
 playNow.addEventListener('click', startgame)
 
+// play.addEventListener('click', plays)
 
 
 
 
-
-
-// if(selectHead() && getNum === 1){
-   
-//     alert('You won')
-   
-// }else if(selectTail() && getNum === 2 ){ 
-//     alert('win')
-// }
-
-// else if(selectHead() && getNum !== 1){
-   
-//     alert('lose')
-// }
-// else if(selectTail() && getNum !== 2){
+// const createImageHead = () => {
  
-//     alert('you lose')
-    
+//     const createHead = document.createElement('img')
+//     createHead.src = 'images/head.png'
+//     createHead.id = "myimg"
+//     imageHead.appendChild(createHead)
 // }
 
+// const createImageTail = () => {
+//     const createTail = document.createElement('img')
+//     createTail.src = 'images/tail.png';
 
+//     createTail.id ="myimg"
 
+//     imageHead.appendChild(createTail)         
+// }
